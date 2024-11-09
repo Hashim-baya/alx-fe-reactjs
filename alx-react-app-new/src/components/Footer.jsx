58d49818a9d7fedf32b11a9a186ee96b48e1b0a7
hashim-baya <bayahashim40@gmail.com>
2024-11-10 00:15:27 +0300

    Added inline styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer style={{textAlign: 'center', fontFamily: 'sans-serif', fontSize: '1rem', color: 'GrayText',  border: '1px solid gray', padding: '10px', margin: '10px' }}>
+        <p>© 2023 City Lovers</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainContent() {
+  return (
+    <main style={{textAlign: "center", fontFamily: 'sans-serif', fontWeight: '400', color: 'rebeccapurple', fontSize: '2rem'}}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  )
+}
+
+export default MainContent;
