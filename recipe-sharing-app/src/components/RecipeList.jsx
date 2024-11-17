4b37dd8952f20cd8a7ebea9e350b29895b9dbd97
hashim-baya <bayahashim40@gmail.com>
2024-11-17 16:14:53 +0300

    Updated

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import useRecipeStore from './RecipeStore';
+import { Link } from 'react-router-dom';
+import useRecipeStore  from './recipeStore';
+import AddRecipeForm from './AddRecipeForm';
 
 const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
+
 
   return (
     <div>
-      {filteredRecipes.length > 0 ? (
-        <ul>
-          {filteredRecipes.map((recipe) => (
-            <li key={recipe.id}>{recipe.name}</li>
-          ))}
-        </ul>
+      <h1>Recipe List</h1>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <Link to={`/recipe/${recipe.id}`}>View Details</Link>
+        </div>
+      ))}
+       {filteredRecipes.length > 0 ? (
+        filteredRecipes.map((recipe) => (
+          <div key={recipe.id} className="recipe-card">
+            <h3>{recipe.name}</h3>
+            <p>{recipe.description}</p>
+          </div>
+        ))
       ) : (
         <p>No recipes found</p>
       )}
+      <AddRecipeForm />
     </div>
   );
 };
