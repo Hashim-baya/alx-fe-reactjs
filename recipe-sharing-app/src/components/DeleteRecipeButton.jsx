38659ae47ad19b63a3c349389cca30217aa3ce7c
hashim-baya <bayahashim40@gmail.com>
2024-11-17 15:51:28 +0300

    Feature: useNaviagte

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,16 @@
 import  useRecipeStore  from './recipeStore';
+import { useNavigate } from 'react-router-dom';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate();
 
   const handleDelete = () => {
     if (window.confirm('Are you sure you want to delete this recipe?')) {
       deleteRecipe(recipeId);
+
+      // Navigate to the recipe list or home after deletion
+      navigate('/');
     }
   };
 
