47ccacd1f4768fc93f29e91b90d72845f40958d4
hashim-baya <bayahashim40@gmail.com>
2024-11-24 23:40:17 +0300

    Updated dynamic routes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,15 +1,15 @@
-import ProtectedRoute from "./components/ProtectedRoute";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import BlogPost from "./components/BlogPost";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/profile">Profile</Link>
+        <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | <Link to="/blog/1">Blog Post</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -21,8 +21,11 @@ function App() {
             </ProtectedRoute>
           }
         />
-        <Route path="/post/:postId" element={<BlogPost />} />
+        {/* Dynamic Route for Blog Post */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
 }
+
+export default App;
